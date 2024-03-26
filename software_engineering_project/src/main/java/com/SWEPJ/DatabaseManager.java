ede51668b288702e44bb74c95720e54321d21fa1
Jaden Amiryar <84093636+dogperson4123@users.noreply.github.com>
2024-03-26 12:41:04 -0400

    database file locks
    database manager now has file locks, can handle concurrency/threading

--- software_engineering_project/src/main/java/com/SWEPJ/DatabaseManager.java
+++ software_engineering_project/src/main/java/com/SWEPJ/DatabaseManager.java
@@ -5,46 +5,74 @@ import java.util.Scanner;
 
 public class DatabaseManager {
     private static String fileName = "Database.txt";
-    private static String lock0str = "0.lock";
-    private static String lock1str = "1.lock";
-    private static String lock2str = "2.lock";
+    private static String lock0str = "0db.lock";
+    private static String lock1str = "1db.lock";
+    private static String lock2str = "2db.lock";
 
-    // 0 = called by entry
+    // 0 = called by entry (in case entry needs to read)
     // 1 = called by review
     // 2 = called by approve
     // ret null if error
+    @SuppressWarnings("resource")
     public static Form read(int db, long PID) {
         try {
             File database = new File(fileName);
             File lock0 = new File(lock0str);
             File lock1 = new File(lock1str);
             File lock2 = new File(lock2str);
-            // File myObj = new File("filename.txt");
-            // myObj.createNewFile();
             Scanner s = new Scanner(database);
+            int test = 0;
+            // MULTITHREAD FILE LOCK
             switch (db) {
                 case 0:
-                    while (lock1.exists() || lock2.exists()) {
-                        Thread.sleep(10000);
-                    }
+                    do {
+                        test = 0;
+                        while (lock1.exists() || lock2.exists()) {
+                            Thread.sleep(1000);
+                        }
+                        lock0.createNewFile();
+                        if (lock1.exists() || lock2.exists())
+                            test = 1;
+                        lock0.delete();
+                    } while (test == 1);
+                    break;
+                case 1:
+                    do {
+                        test = 0;
+                        while (lock0.exists() || lock2.exists()) {
+                            Thread.sleep(1000);
+                        }
+                        lock1.createNewFile();
+                        if (lock0.exists() || lock2.exists())
+                            test = 1;
+                        lock1.delete();
+                    } while (test == 1);
+                    break;
+                case 2:
+                    do {
+                        test = 0;
+                        while (lock1.exists() || lock0.exists()) {
+                            Thread.sleep(1000);
+                        }
+                        lock2.createNewFile();
+                        if (lock1.exists() || lock0.exists())
+                            test = 1;
+                        lock2.delete();
+                    } while (test == 1);
+                    break;
+                default:
+                    throw new Exception("Invalid access number");
 
             }
-            File lock = new File(Integer.toString(db) + ".lock");
-            int test = 0;
-            lock.createNewFile();
-
-            do {
-                test = 0;
-            } while (test > 0);
-
+            File currLock = new File(Integer.toString(db) + "db.lock");
             while (s.hasNextLine()) {
-                System.out.println(s.nextLine());
+                System.out.println(s.nextLine()); // WRITE READING CODE HERE
             }
             s.close();
+            currLock.delete();
             return null;
         } catch (Exception e) {
             System.out.println("read failed");
-
             return null;
         }
     }
@@ -53,13 +81,65 @@ public class DatabaseManager {
     // 1 = called by review
     // 2 = called by approve
     // ret false if error
-    public static boolean save() {
+    public static boolean save(int db) {
         try {
             File database = new File(fileName);
+            File lock0 = new File(lock0str);
+            File lock1 = new File(lock1str);
+            File lock2 = new File(lock2str);
             Scanner s = new Scanner(database);
+            int test = 0;
+            // MULTITHREAD FILE LOCK
+            switch (db) {
+                case 0:
+                    do {
+                        test = 0;
+                        while (lock1.exists() || lock2.exists()) {
+                            Thread.sleep(1000);
+                        }
+                        lock0.createNewFile();
+                        if (lock1.exists() || lock2.exists())
+                            test = 1;
+                        lock0.delete();
+                    } while (test == 1);
+                    break;
+                case 1:
+                    do {
+                        test = 0;
+                        while (lock0.exists() || lock2.exists()) {
+                            Thread.sleep(1000);
+                        }
+                        lock1.createNewFile();
+                        if (lock0.exists() || lock2.exists())
+                            test = 1;
+                        lock1.delete();
+                    } while (test == 1);
+                    break;
+                case 2:
+                    do {
+                        test = 0;
+                        while (lock1.exists() || lock0.exists()) {
+                            Thread.sleep(1000);
+                        }
+                        lock2.createNewFile();
+                        if (lock1.exists() || lock0.exists())
+                            test = 1;
+                        lock2.delete();
+                    } while (test == 1);
+                    break;
+                default:
+                    throw new Exception("Invalid access number");
+
+            }
+            File currLock = new File(Integer.toString(db) + ".lock");
+            while (s.hasNextLine()) {
+                System.out.println(s.nextLine()); // WRITE SAVING CODE HERE
+            }
             s.close();
+            currLock.delete();
             return true;
         } catch (Exception e) {
+            System.out.println("read failed");
             return false;
         }
     }
