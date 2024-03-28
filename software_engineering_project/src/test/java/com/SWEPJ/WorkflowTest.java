c0f6ede7cc237aa2360542f12d7c11ba88eae466
Jaden Amiryar <84093636+dogperson4123@users.noreply.github.com>
2024-03-28 03:04:45 -0400

    workflow tests
    will finish tomorrow

--- software_engineering_project/src/test/java/com/SWEPJ/WorkflowTest.java
+++ software_engineering_project/src/test/java/com/SWEPJ/WorkflowTest.java
@@ -7,16 +7,141 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.Scanner;
 
 import org.junit.After;
 import org.junit.Test;
+import org.junit.Ignore;
 
 public class WorkflowTest {
+
+    @Ignore
     @Test
-    public void testInitalizePID() {
+    public void testReviewAccess() {
+        Form[] test = WorkflowManager.info(1);
+        Boolean[] fails = test[0].getFail();
+        for (int i = 0; i < 9; i++) {
+            if (fails[i])
+                assertTrue(false);
+        }
+        if (test[0].getPID() != 1111111111)
+            assertTrue(false);
+        if (!(test[0].getRelation()[1].equals("father")))
+            assertTrue(false);
+        if (!(test[0].getDOB().equals("1/1/1990")))
+            assertTrue(false);
+        if (!(test[0].getDOD().equals("NULL")))
+            assertTrue(false);
+        if (!(test[0].getFirstName().equals("Jill")))
+            assertTrue(false);
+        if (!(test[0].getMiddleName().equals("NULL")))
+            assertTrue(false);
+        if (!(test[0].getLastName().equals("Doe")))
+            assertTrue(false);
+        if (!(test[0].getEmail().equals("jill@doe.com")))
+            assertTrue(false);
+        if (test[0].getRelatedPID()[1] != 333333333)
+            assertTrue(false);
+        fails = test[1].getFail();
+        for (int i = 0; i < 9; i++) {
+            if (fails[i])
+                assertTrue(false);
+        }
+        if (test[0].getPID() != 333333333)
+            assertTrue(false);
+        if (!(test[1].getRelation()[0].equals("")))
+            assertTrue(false);
+        if (!(test[1].getDOB().equals("1/1/1970")))
+            assertTrue(false);
+        if (!(test[1].getDOD().equals("12/30/2012")))
+            assertTrue(false);
+        if (!(test[1].getFirstName().equals("John")))
+            assertTrue(false);
+        if (!(test[1].getMiddleName().equals("Scott")))
+            assertTrue(false);
+        if (!(test[1].getLastName().equals("Doe")))
+            assertTrue(false);
+        if (!(test[1].getEmail().equals("john@doe.com")))
+            assertTrue(false);
+        File currLock;
+        for (int i = 0; i < 3; i++) {
+            currLock = new File(Integer.toString(i) + "db.lock");
+            if (currLock.exists())
+                assertTrue(false);
+        }
+        assertTrue(true);
+    }
+
+    @Ignore
+    @Test
+    public void testApproveAccess() {
+        Form[] test = WorkflowManager.info(2);
+        Boolean[] fails = test[0].getFail();
+        for (int i = 0; i < 9; i++) {
+            if (fails[i])
+                assertTrue(false);
+        }
+        if (test[0].getPID() != 1111111111)
+            assertTrue(false);
+        if (!(test[0].getRelation()[1].equals("father")))
+            assertTrue(false);
+        if (!(test[0].getDOB().equals("1/1/1990")))
+            assertTrue(false);
+        if (!(test[0].getDOD().equals("NULL")))
+            assertTrue(false);
+        if (!(test[0].getFirstName().equals("Jill")))
+            assertTrue(false);
+        if (!(test[0].getMiddleName().equals("NULL")))
+            assertTrue(false);
+        if (!(test[0].getLastName().equals("Doe")))
+            assertTrue(false);
+        if (!(test[0].getEmail().equals("jill@doe.com")))
+            assertTrue(false);
+        if (test[0].getRelatedPID()[1] != 333333333)
+            assertTrue(false);
+        fails = test[1].getFail();
+        for (int i = 0; i < 9; i++) {
+            if (fails[i])
+                assertTrue(false);
+        }
+        if (test[0].getPID() != 333333333)
+            assertTrue(false);
+        if (!(test[1].getRelation()[0].equals("")))
+            assertTrue(false);
+        if (!(test[1].getDOB().equals("1/1/1970")))
+            assertTrue(false);
+        if (!(test[1].getDOD().equals("12/30/2012")))
+            assertTrue(false);
+        if (!(test[1].getFirstName().equals("John")))
+            assertTrue(false);
+        if (!(test[1].getMiddleName().equals("Scott")))
+            assertTrue(false);
+        if (!(test[1].getLastName().equals("Doe")))
+            assertTrue(false);
+        if (!(test[1].getEmail().equals("john@doe.com")))
+            assertTrue(false);
+        File currLock;
+        for (int i = 0; i < 3; i++) {
+            currLock = new File(Integer.toString(i) + "db.lock");
+            if (currLock.exists())
+                assertTrue(false);
+        }
+        assertTrue(true);
+    }
+
+    @Ignore
+    @Test
+    public void testEntryPush() {
+        long PID = 111111111;
+        long[] related = { 222222222 };
+        String[] relation = { "tester" };
+        Form tester = new Form(PID, "1/1/2000", "NULL", relation, "First", "Middle", "last", "email@address.com",
+                related);
+        Boolean result = WorkflowManager.update(0, tester, 1);
+        File workflow = new File("Workflow.txt");
         try {
-            // DatabaseManager.read();
-            assertTrue(true);
+            Scanner s = new Scanner(workflow);
+            // continue here
         } catch (Exception e) {
             assertTrue(false);
         }
