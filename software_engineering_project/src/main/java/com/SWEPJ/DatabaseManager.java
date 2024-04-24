f3e12774334b2089b4ff3b53d1d844a4e41d9592
anders-ogelman <134874009+anders-ogelman@users.noreply.github.com>
2024-04-24 19:17:09 -0400

    implementing further functionality on entry stage submit button, began implementing database update

--- software_engineering_project/src/main/java/com/SWEPJ/DatabaseManager.java
+++ software_engineering_project/src/main/java/com/SWEPJ/DatabaseManager.java
@@ -2,163 +2,54 @@ package com.SWEPJ;
 
 import java.io.File;
 import java.util.Scanner;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class DatabaseManager {
-    private static String fileName = "Database.txt";
+	
+	private static String fileName = "Database.txt";
 
-    // PID = pid of requester
-    // returns all the forms submitted by the requester
-    public static Form fetch(long FID) {
-        File database = new File(fileName);
+	// PID = pid of requester
+	// returns all the forms submitted by the requester
+	public static Form fetch(long FID) {
+		File database = new File(fileName);
 
-        try {
-            Scanner s = new Scanner(database);
-        } catch (Exception e) {
-            return null;
-        }
-        return null;
+		try {
+			Scanner s = new Scanner(database);
+		} catch (Exception e) {
+			return null;
+		}
+		return null;
+	}
 
-        /*
-         * Thread.sleep(10000);
-         * File database = new File(fileName);
-         * File lock0 = new File(lock0str);
-         * File lock1 = new File(lock1str);
-         * File lock2 = new File(lock2str);
-         * Scanner s = new Scanner(database);
-         * int test = 0;
-         * // MULTITHREAD FILE LOCK
-         * if (skip == false) {
-         * switch (db) {
-         * case 0:
-         * do {
-         * test = 0;
-         * while (lock1.exists() || lock2.exists()) {
-         * Thread.sleep(1000);
-         * }
-         * lock0.createNewFile();
-         * if (lock1.exists() || lock2.exists()) {
-         * test = 1;
-         * lock0.delete();
-         * }
-         * } while (test == 1);
-         * break;
-         * case 1:
-         * do {
-         * test = 0;
-         * while (lock0.exists() || lock2.exists()) {
-         * Thread.sleep(1000);
-         * }
-         * lock1.createNewFile();
-         * if (lock0.exists() || lock2.exists()) {
-         * test = 1;
-         * lock1.delete();
-         * }
-         * } while (test == 1);
-         * break;
-         * case 2:
-         * do {
-         * test = 0;
-         * while (lock1.exists() || lock0.exists()) {
-         * Thread.sleep(1000);
-         * }
-         * lock2.createNewFile();
-         * if (lock1.exists() || lock0.exists()) {
-         * test = 1;
-         * lock2.delete();
-         * }
-         * } while (test == 1);
-         * break;
-         * default:
-         * throw new Exception("Invalid access number");
-         * 
-         * }
-         * }
-         * File currLock = new File(Integer.toString(db) + "db.lock");
-         * while (s.hasNextLine()) {
-         * System.out.println(s.nextLine()); // WRITE READING CODE HERE
-         * }
-         * s.close();
-         * currLock.delete();
-         * return null;
-         * } catch (Exception e) {
-         * System.out.println("read failed");
-         * return null;
-         * }
-         */
-    }
+	// form = form being saved
+	// ret false if error
+	// ret true if worked
 
-    // form = form being saved
-    // ret false if error
-    // ret true if worked
+	public static boolean update(Form form) {
+		
+		//only implementing the adding new form functionality - somebody else will add updating
+		if(form.getFID() == -1) {
+			
 
-    public static boolean update(Form form) {
-        return true;
-        /*
-         * return
-         * try {
-         * File database = new File(fileName);
-         * File lock0 = new File(lock0str);
-         * File lock1 = new File(lock1str);
-         * File lock2 = new File(lock2str);
-         * Scanner s = new Scanner(database);
-         * int test = 0;
-         * // MULTITHREAD FILE LOCK
-         * switch (db) {
-         * case 0:
-         * do {
-         * test = 0;
-         * while (lock1.exists() || lock2.exists()) {
-         * Thread.sleep(1000);
-         * }
-         * lock0.createNewFile();
-         * if (lock1.exists() || lock2.exists()) {
-         * test = 1;
-         * lock0.delete();
-         * }
-         * } while (test == 1);
-         * break;
-         * case 1:
-         * do {
-         * test = 0;
-         * while (lock0.exists() || lock2.exists()) {
-         * Thread.sleep(1000);
-         * }
-         * lock1.createNewFile();
-         * if (lock0.exists() || lock2.exists()) {
-         * test = 1;
-         * lock1.delete();
-         * }
-         * } while (test == 1);
-         * break;
-         * case 2:
-         * do {
-         * test = 0;
-         * while (lock1.exists() || lock0.exists()) {
-         * Thread.sleep(1000);
-         * }
-         * lock2.createNewFile();
-         * if (lock1.exists() || lock0.exists()) {
-         * test = 1;
-         * lock2.delete();
-         * }
-         * } while (test == 1);
-         * break;
-         * default:
-         * throw new Exception("Invalid access number");
-         * 
-         * }
-         * File currLock = new File(Integer.toString(db) + ".lock");
-         * while (s.hasNextLine()) {
-         * System.out.println(s.nextLine()); // WRITE SAVING CODE HERE
-         * }
-         * s.close();
-         * currLock.delete();
-         * return true;
-         * } catch (Exception e) {
-         * System.out.println("read failed");
-         * return false;
-         * }
-         * }
-         */
-    }
+		}
+
+		return true;
+	}
+
+
+    	private void resetDatabase() {
+
+		try {
+		    Path replaceMe = Paths.get("Database.txt");
+		    Path replacement = Paths.get("backup/Database.txt");
+		    Files.copy(replacement, replaceMe, StandardCopyOption.REPLACE_EXISTING);
+		} catch (Exception e) {
+		    System.err.println("manual database replacement required");
+		}
+	}
+
 }
--- software_engineering_project/src/main/java/com/SWEPJ/Entry.java
+++ software_engineering_project/src/main/java/com/SWEPJ/Entry.java
@@ -7,7 +7,12 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
+
+
+//plan: accepting input, use it to create a form, call the requisite method from database manager
 
 public class Entry {
 	public Entry() {
@@ -31,18 +36,53 @@ public class Entry {
 		Label dodLabel = new Label("Date of Death:");
 		TextField dodField = new TextField();
 
+		Label pidLabel = new Label("PID: ");
+		TextField pidField = new TextField();
+
 		Button submitButton = new Button("Submit");
+
+		//handling form submission
+		EventHandler<ActionEvent> submitForm = new EventHandler<ActionEvent>() {
+			
+			public void handle(ActionEvent e) {
+				//debug message
+				System.out.println("pressed button!");
+				
+				//putting them all in variables to make validation easier down the line
+				long pid = Long.parseLong(pidField.getText());
+				String dob = dobField.getText();
+				String dod = dodField.getText();
+				String firstName = firstNameField.getText();
+				String middleName = middleNameLabel.getText();
+				String lastName = lastNameField.getText();
+				String email = emailField.getText();
+
+				
+				//TODO: add in basic validation, figure out what to do
+				//with placeholders 00000 and placeholder relation. these fields may get removed
+				Form submissionForm = new Form(pid, dob, dod, "placeholder relation", firstName, middleName, lastName, email, 0000000);
+				
+
+				//Adding new form to the database
+				DatabaseManager.update(submissionForm);
+			}
+
+		};
+
+		submitButton.setOnAction(submitForm);
 		
 		//JavaFx UI VBox layout setup
 		Stage secondStage = new Stage();
 
 		VBox entryBox = new VBox(4, new Label("Entry Window"));
 		entryBox.getChildren().addAll(firstNameLabel, firstNameField, middleNameLabel, middleNameField, lastNameLabel,
-				lastNameField, emailLabel, emailField, dobLabel, dobField, dodLabel, dodField, submitButton);
+				lastNameField, emailLabel, emailField, dobLabel, dobField, dodLabel, dodField, pidLabel, pidField, submitButton);
+
 		
 		//adding the layout to the screen and rendering
 		Scene entryScene = new Scene(entryBox);
 		secondStage.setScene(entryScene);
 		secondStage.show();
 	}
+
 }
